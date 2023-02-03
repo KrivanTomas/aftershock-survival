18a0efa7a97bbf5e88b7dbca846077f1bef57357
KrivanTomas <krivant.05@spst.eu>
2023-02-03 22:09:57 +0100

    UPR

--- Assets/Scripts/DebugShooting.cs
+++ Assets/Scripts/DebugShooting.cs
@@ -28,7 +28,7 @@ public class DebugShooting : MonoBehaviour
     void Start()
     {
         gunshotsClips = gunshots.Length - 1;
-        DOTween.Init(null, false, LogBehaviour.Verbose);
+        DOTween.Init(null, false, LogBehaviour.ErrorsOnly);
     }
 
     // Update is called once per frame
