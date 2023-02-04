79fad2805dd7f0427097d98300f973f9f7e69343
VojtechMensik <mensikv.03@spst.eu>
2023-02-04 20:17:01 +0100

    Nastavení velikosti okna inv
    automaticky nastavuje velikost okna invetáře podle velikosti objektu InventorySlot a podle počtů slotů jako parametr
    okno je umístěné na střed obrazovky

--- Assets/InventoryCreation.cs
+++ Assets/InventoryCreation.cs
@@ -2,23 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class InventoryCreation : MonoBehaviour
 {
     //představuje inventář jako celek - spravuje item sloty
     [SerializeField] private GameObject inventory;
     [SerializeField] private GameObject itemSlot;
-    private GridLayoutGroup grid;
-
+    private RectTransform invtrans;
+    private RectTransform itmtrans;
+    private GridLayoutGroup gridLayout;
     public bool otestuj = false;
     public int PocetSlotySirka;
     public int PocetSlotyVyska;
     void Start()
     {
+        invtrans = inventory.GetComponent<RectTransform>();
+        itmtrans = itemSlot.GetComponent<RectTransform>();
+        gridLayout = inventory.GetComponent<GridLayoutGroup>();
     }
     private void Update() {
         if(otestuj)
         Inicializuj();
         otestuj = false;
+        //Debug.Log(invtrans.rect);
     }
     private void Inicializuj()
     {
@@ -32,10 +38,21 @@ public class InventoryCreation : MonoBehaviour
         //(right - left) - šířka okna
         //(bottom - top) - výška okna
         
-        //vytvoření
+        //vytvoření slotů inventáře
+        
         int pocet = PocetSlotySirka*PocetSlotyVyska;
         for(int i = 0;i<pocet;i++)
-            Instantiate(itemSlot,inventory.transform);
+        {
+            Instantiate(itemSlot,inventory.transform);            
+           
+        }
+        
+        //nastavení velikosti okna inventáře
+        /*
+        šířka = gridLayout.padding.horizontal + itmtrans.rect.width * PocetSlotySirka;
+        výška = gridLayout.padding.vertical + itmtrans.rect.height * PocetSlotyVyska;
+        */
+        invtrans.sizeDelta = new Vector2(gridLayout.padding.horizontal + itmtrans.rect.width * PocetSlotySirka,gridLayout.padding.vertical + itmtrans.rect.height * PocetSlotyVyska);
         
     }
 }
