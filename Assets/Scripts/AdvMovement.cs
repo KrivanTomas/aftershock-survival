57740605efa1fc8c704b374530be2da3bd49dad0
KrivanTomas <krivant.05@spst.eu>
2023-02-23 17:53:16 +0100

    Basic procedural weapon
    With IK and code (no buggy animations)

--- Assets/Scripts/AdvMovement.cs
+++ Assets/Scripts/AdvMovement.cs
@@ -20,7 +20,7 @@ public class AdvMovement : MonoBehaviour
     [SerializeField] private CharacterController characterController;
     [SerializeField] private AudioSource movementAudioSource;
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private Transform view;[Header("Settings")]
+    [SerializeField] private Transform view;
 
     [Header("Settings")]
     [SerializeField] [Range(0f,1f)] private float cameraOffset = 0.3f;
@@ -38,19 +38,23 @@ public class AdvMovement : MonoBehaviour
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private LayerMask checkHeadMask;
 
-    [Header("Settings")]
+    [Header("SFX")]
     [SerializeField] private AudioClip walkSound;
     [SerializeField] private AudioClip runSound;
 
+    [Header("Smoothing")]
+    private float strafeVelocity = 0f;
+    [SerializeField] private float strafeMaxSpeed = 1f;
+    [SerializeField] private float strafeTime = 1f;
+    [SerializeField] private float strafeMultiplier = 1f;
 
 
-    private enum MoveState{
+    private enum MoveState {
         Walking,
         Runing,
         Crouching,
         NotSet
     }
-
 
 
     //
@@ -63,33 +67,41 @@ public class AdvMovement : MonoBehaviour
     private float verticalRotation = 0f;
     private float speedMod;
     private bool isGrounded = false;
+
+    private float targetStrafeRotation;
     
     private void Temp() {
         Cursor.lockState = CursorLockMode.Locked;
     }
-
 
     private void Start() {
         Temp();
     }
 
     private void Update() {
+        // Move camera with mouse
         MoveView();
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistanse, groundMask);
 
+
+        // Set values sepending on a move state (run/walk/..)
+        // Crouch
         if (Input.GetKey(KeyCode.LeftControl) && moveState != MoveState.Crouching) {
             SetHeight(crouchHeight);
             speedMod = crouchSpeed;
             movementAudioSource.clip = walkSound;
             moveState = MoveState.Crouching;
         }
-        else if (Input.GetKey(KeyCode.LeftShift) && moveState != MoveState.Runing) {
+        // Sprint
+        else if (Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl) && moveState != MoveState.Runing) {
             if(TrySetHeight(baseHeight)){
                 speedMod = sprintSpeed;
                 movementAudioSource.clip = runSound;
                 moveState = MoveState.Runing;
             }
         }
+        // Walk
         else if (!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl) && moveState != MoveState.Walking) {
             if(TrySetHeight(baseHeight)){
                 speedMod = baseSpeed;
@@ -98,9 +110,16 @@ public class AdvMovement : MonoBehaviour
             }
         }
 
-        Vector3 inputMovement = Vector3.ClampMagnitude(Input.GetAxis("Vertical") * transform.forward + Input.GetAxis("Horizontal") * transform.right, 1f);
+        // Get movement from input (smoothed)
+        Vector3 inputMovement = Vector3.ClampMagnitude(Input.GetAxis("Vertical") * view.forward + Input.GetAxis("Horizontal") * view.right, 1f);
         inputMovement *= speedMod;
 
+        // Rotate camera when strafing
+        Quaternion oldquat = view.localRotation;
+        Vector3 old = oldquat.eulerAngles;
+        view.transform.localRotation = Quaternion.Euler(old.x, old.y, Mathf.SmoothDampAngle(old.z, -Input.GetAxis("Horizontal") * speedMod * strafeMultiplier, ref strafeVelocity, strafeTime, strafeMaxSpeed));
+
+        // Stop/Play audio
         if((inputMovement == Vector3.zero || !isGrounded) && movementAudioSource.isPlaying){
             movementAudioSource.Stop();
         }
@@ -108,30 +127,34 @@ public class AdvMovement : MonoBehaviour
             movementAudioSource.Play();
         }
         
+        // Ground movement
         if(isGrounded && velocity.y < 0){
             velocity.y = staticGravity;
             movement = inputMovement;
+
+            // Add velocity on jump
             if(Input.GetButtonDown("Jump")){
                 velocity.y = Mathf.Sqrt(jumpHeight * -3f * gravity);
             }
         }
 
+        // Apply gravity and send to characterController
         velocity.y += gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime + movement * Time.deltaTime);
     }   
 
-    private void MoveView() {
+    private void MoveView() { // Rotates the camera according to mouse movements (also clamps vertical rotation)
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
         verticalRotation -= mouseY;
         verticalRotation = Mathf.Clamp(verticalRotation,-90f,90f);
 
-        view.transform.localRotation = Quaternion.Euler(verticalRotation,0f,0f);
-        playerGameObject.transform.eulerAngles += new Vector3(0f,mouseX,0f);
+        view.transform.localRotation = Quaternion.Euler(verticalRotation,view.localEulerAngles.y + mouseX,view.localEulerAngles.z);
+        //playerGameObject.transform.eulerAngles += new Vector3(0f,mouseX,0f);
     }
 
-    private bool TrySetHeight(float height) {
+    private bool TrySetHeight(float height) { // Checks if there is space above before doing SetHeight()
         float radius = characterController.radius;
         bool isBlocked = Physics.CheckSphere(
             playerGameObject.transform.TransformPoint(Vector3.up * Mathf.Max(height - radius*2, 0f)),
@@ -144,10 +167,57 @@ public class AdvMovement : MonoBehaviour
         return false;
     }
 
-    private void SetHeight(float height){
+    private void SetHeight(float height){ // Sets the collider and camera to the desired height
         float radius = characterController.radius;
         characterController.height = height;
         characterController.center = Vector3.up * (.5f * (Mathf.Max((height - radius*2),0f) + groundOffset));
         view.transform.localPosition = Vector3.up * (Mathf.Max(height - radius*2, 0f) + cameraOffset * radius);
-    }   
+    }  
 }
+
+// public class Tween {
+//     private float t;
+
+//     private float clamp;
+//     private float transitionTime;
+
+
+//     private float target_a;
+//     private float target_b;
+
+//     public Tween (float a, float b, float transition) {
+//         this.t = 0;
+//         this.target_a = a;
+//         this.target_b = b;
+//         this.transitionTime = transition;
+//     }
+
+//     public Tween SetTargetA(float a){
+//         this.target_a = a;
+//         return this;
+//     }
+
+//     public Tween SetTargetA(float a, bool reset_t){
+//         this.target_a = a;
+//         if(reset_t) this.t = 0;
+//         return this;
+//     }
+
+//     public Tween SetTargetB(float b){
+//         this.target_b = b;
+//         return this;
+//     }
+
+//     public Tween SetTargetB(float b, bool reset_t){
+//         this.target_b = b;
+//         if(reset_t) this.t = 0;
+//         return this;
+//     }
+
+
+//     public float Run() {
+//         this.t += 1 / transitionTime * Time.deltaTime;
+//         return Mathf.Lerp(this.target_a, this.target_b, this.t);
+//     }
+
+// }
--- Assets/Scripts/AdvWeapon.cs
+++ Assets/Scripts/AdvWeapon.cs
@@ -4,88 +4,48 @@ using UnityEngine;
 
 public class AdvWeapon : MonoBehaviour
 {
-    [SerializeField] private GameObject pistolGameObject;
-    [SerializeField] private Animator animator;
-    [SerializeField] private AudioSource audioSource;
-    [SerializeField] private Transform barrel;
-    [SerializeField] private Light lightFlash;
-
-
-    [SerializeField] private AudioClip gunshot;
-
+    [SerializeField] private Transform weapon;
+    [SerializeField] private Transform weaponFloat;
+    [SerializeField] private Transform hipPosition;
+    [SerializeField] private Transform aimPosition;
 
     [Header("Settings")]
-    [SerializeField] private int maxBulletsInClip = 7;
+
+    [SerializeField] private Vector3 weaponKick = Vector3.up;
 
 
+    private Vector3 aimVelocity = Vector3.zero;
+    [SerializeField] private float aimTime = 1f;
+    [SerializeField] private float aimMaxSpeed = 1f;
 
-    private int bulletsInClip;
-    private bool pistolEquiped = true;
-    private bool reloading = false;
-    private bool helpu = false;
+
+    private bool isAiming = false;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        bulletsInClip = maxBulletsInClip;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!pistolEquiped) return;
-        
-        lightFlash.intensity -= 10 * Time.deltaTime;
 
 
-        animator.SetBool("Aim", Input.GetKey(KeyCode.Mouse1));
-
-        if(Input.GetKeyDown(KeyCode.Mouse0) && !animator.GetCurrentAnimatorStateInfo(0).IsName("Shoot") && bulletsInClip > 0 && !reloading){
-            animator.SetTrigger("Shoot");
-            audioSource.pitch = Random.Range(1f,1.1f);
-            audioSource.PlayOneShot(gunshot);
-            bulletsInClip--;
-            lightFlash.intensity = 1;
-            Debug.DrawRay(barrel.transform.position, barrel.transform.forward * 10, Color.red, 3);
-
-            RaycastHit hit;
-            if(Physics.Raycast(barrel.transform.position, barrel.transform.forward, out hit, 300)){
-                if(hit.transform.gameObject.tag == "Shootable"){
-                    hit.rigidbody.AddForceAtPosition(barrel.transform.forward * 3, hit.point, ForceMode.Impulse);
-                }
-            }
+        Transform finalPos = hipPosition;
+        isAiming = false;
+        if(Input.GetMouseButton(1)) {
+            finalPos = aimPosition;
+            isAiming = true;
         }
 
-        if(reloading){
-            bool desudesu = animator.GetCurrentAnimatorStateInfo(0).IsName("reload") || animator.GetCurrentAnimatorStateInfo(0).IsName("reloadNoammo");
-            if(desudesu || helpu) {
-                helpu = true;
-                reloading = desudesu;
-            }
-            if(!reloading){
-                bulletsInClip = maxBulletsInClip;
-            }
+        if(Input.GetMouseButtonDown(0)){
+            weapon.position += weapon.transform.TransformVector(weaponKick);
         }
 
-        if(Input.GetKeyDown(KeyCode.R) && !reloading){
-            animator.SetBool("Empty", bulletsInClip == 0);
-            animator.SetTrigger("Reload");
-            bulletsInClip = 0;
-            reloading = true;
-            helpu = false;
-        }
-
-    }
-
-    public void EquipPistol(){
-        pistolGameObject.SetActive(true);
-        pistolEquiped = true;
-    }
-    public void DeEquipPistol(){
-        animator.SetTrigger("deDraw");
-        pistolGameObject.SetActive(false);
-        pistolEquiped = false;
+        weapon.position = Vector3.SmoothDamp(weapon.position, finalPos.position, ref aimVelocity, aimTime, aimMaxSpeed * (isAiming ? 10 : 1));
+        weapon.rotation = finalPos.rotation;
     }
 }
