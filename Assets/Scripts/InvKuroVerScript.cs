f665c4c69aaeba3623c9e0e3fa03a7fa1758c160
KrivanTomas <krivant.05@spst.eu>
2023-02-27 17:55:46 +0100

    Auto fill

--- Assets/Scripts/InvKuroVerScript.cs
+++ Assets/Scripts/InvKuroVerScript.cs
@@ -10,12 +10,21 @@ public class InvKuroVerScript : MonoBehaviour, IPointerDownHandler, IPointerUpHa
     [Header("Settings")]
     [SerializeField] private Vector2 gridSize = new Vector2(10f,10f);    
     [SerializeField] private GameObject itemPrefab;    
-    [SerializeField] private KuroItem testItemData1;    
-    [SerializeField] private KuroItem testItemData2;    
+    [SerializeField] private KuroItem[] testItemData;    
+
+    [SerializeField] private Color defaultColor;
+    [SerializeField] private Color collisionColor;
+
+    private Vector3 whtOffset;
+
+    private float tileSize;
 
     private GameObject dragGameObject;
     private KuroItemHandle dragItemHandle;
     private RectTransform inventoryRectTransform;
+    private Vector3 lastPosition;
+    private bool lastRotation = false;
+    private bool collided = false;
 
     private Rect inventoryBounds;
 
@@ -25,15 +34,30 @@ public class InvKuroVerScript : MonoBehaviour, IPointerDownHandler, IPointerUpHa
     void Start(){
         inventoryRectTransform = gameObject.GetComponent<RectTransform>();
         inventoryBounds = inventoryRectTransform.rect;
+        tileSize = inventoryBounds.size.x / gridSize.x;
+        whtOffset = new Vector3(tileSize * .5f, inventoryBounds.height * .5f + tileSize * .5f, 0f);
 
-        for(int i = 0; i < 10; i++){
-        items.Add(AddItemTest(testItemData1));
+        for(int i = 0; i < 40; i++){
+            KuroItemHandle itemHandle;
+            if(TryAddItem(testItemData[Random.Range(0, testItemData.GetLength(0) - 1)], out itemHandle)){
+                items.Add(itemHandle);
+            }
+            else{
+                Debug.Log("<color=red>Item dosent fit " + i + "</color>");
+            }
+            
         }
-        items.Add(AddItemTest(testItemData2));
+        
     }
 
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Semicolon)){
+            KuroItemHandle itemHandle;
+            if(TryAddItem(testItemData[Random.Range(0, testItemData.GetLength(0) - 1)], out itemHandle)){
+                items.Add(itemHandle);
+            }
+        }
         // Move dragged GameObject while snapping to grid
         Vector2 localPoint;
         
@@ -42,18 +66,17 @@ public class InvKuroVerScript : MonoBehaviour, IPointerDownHandler, IPointerUpHa
             if(Input.GetKeyDown(KeyCode.R)){
                 dragItemHandle.Rotate();
             }
-            float tileSize = inventoryBounds.size.x / gridSize.x;
             // Shift the grid for odd axis
-            Vector3 oddOffset = 
-                Vector3.right * (dragItemHandle.finalSize.x % 2 == 0 ? tileSize * 0.5f : 0f) +
-                Vector3.up * (dragItemHandle.finalSize.y % 2 == 0 ? tileSize * 0.5f : 0f);
+            Vector3 oddOffset = CalculateOddOffset(dragItemHandle);
 
-            dragGameObject.transform.localPosition = SnapToGrid( localPoint - (Vector2)oddOffset, tileSize, tileSize) + oddOffset;
-            if(VeryBadlyImplementedCollistionCheck(dragItemHandle)){
-                dragItemHandle.background.color = Color.red;
+            dragGameObject.transform.localPosition = SnapToGrid( localPoint - (Vector2)oddOffset - (Vector2)whtOffset, tileSize, tileSize) + oddOffset + whtOffset;
+            if(VeryBadlyImplementedCollistionCheck(dragItemHandle) || OutOfBoundsCheck(dragItemHandle, inventoryRectTransform)){
+                collided = true;
+                dragItemHandle.background.color = collisionColor;
             }
             else {
-                dragItemHandle.background.color = Color.white;
+                collided = false;
+                dragItemHandle.background.color = defaultColor;
             }
         }
     }
@@ -62,17 +85,24 @@ public class InvKuroVerScript : MonoBehaviour, IPointerDownHandler, IPointerUpHa
 
     public void OnPointerDown(PointerEventData poi){
         // Check for valid GameObject (item) to drag
+        collided = false;
         if(poi.button == PointerEventData.InputButton.Left) {
             GameObject hitGameObject = poi.pointerCurrentRaycast.gameObject;
             if(hitGameObject.tag == "InventoryItem"){
                 dragGameObject = hitGameObject;
                 dragItemHandle = dragGameObject.GetComponent<KuroItemHandle>();
+                lastPosition = dragGameObject.transform.position;
+                lastRotation = dragItemHandle.rotated;
                 dragGameObject.transform.SetAsLastSibling();
             }
         }
     }
     public void OnPointerUp(PointerEventData poi){
-
+        if(collided && dragGameObject){
+            dragGameObject.transform.position = lastPosition;
+            dragItemHandle.background.color = defaultColor;
+            dragItemHandle.Rotate(lastRotation);
+        }
         // Forget about dragged GameObject
         dragGameObject = null;
         dragItemHandle = null;
@@ -84,6 +114,10 @@ public class InvKuroVerScript : MonoBehaviour, IPointerDownHandler, IPointerUpHa
             if(RectOverlap(item.rectTransform, item.rotated, otherItem.rectTransform, otherItem.rotated)) return true;
         }
         return false;
+    }
+
+    private bool OutOfBoundsCheck(KuroItemHandle itemHandle, RectTransform inventoryTransform){
+        return !RectContains(inventoryRectTransform, itemHandle.rectTransform, itemHandle.rotated);
     }
 
     private bool RectOverlap(RectTransform firstRect, bool firstRotated, RectTransform secondRect, bool secondRotated) // rect.Overlap() is thrash
@@ -111,14 +145,73 @@ public class InvKuroVerScript : MonoBehaviour, IPointerDownHandler, IPointerUpHa
         return true;
     }
 
-    private KuroItemHandle AddItemTest(KuroItem itemData){
+    private bool RectContains(RectTransform containerRect, RectTransform containedRect, bool containedRotated)
+    {
+        float firstWidth = containerRect.rect.width;
+        float firstHeight = containerRect.rect.height;
+        float secondWidth = !containedRotated ? containedRect.rect.width : containedRect.rect.height;
+        float secondHeight = containedRotated ? containedRect.rect.width : containedRect.rect.height;
+        
+        if (containedRect.localPosition.x + secondWidth * .5f > firstWidth * .5f || containedRect.localPosition.x - secondWidth * .5f < -firstWidth * .5f)
+        {
+            return false;
+        }
+        if (containedRect.localPosition.y + secondHeight * .5f > firstHeight * .5f || containedRect.localPosition.y - secondHeight * .5f < -firstHeight * .5f)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private bool TryAddItem(KuroItem itemData, out KuroItemHandle itemHandle){
+        // speed up conditions:
+        //  - in inv bounds
+        //  - not in the same position
+        //  -
+
+        itemHandle = AddItem(itemData);
+
+        Vector3 oddOffset = 
+                Vector3.right * (itemHandle.finalSize.x % 2 == 0 ? tileSize * 0.5f : 0f) +
+                Vector3.up * (itemHandle.finalSize.y % 2 == 0 ? tileSize * 0.5f : 0f);
+
+        for(int row = 0; row < gridSize.x; row++){
+            for(int column = 0; column < gridSize.x; column++){ 
+                Vector3 scanPos = new Vector3(tileSize * column - inventoryBounds.width * .5f + tileSize * .5f, tileSize * -row + inventoryBounds.height * .5f - tileSize * .5f, 0f);
+                itemHandle.rectTransform.localPosition = scanPos + CalculateOddOffset(itemHandle);
+                if(!VeryBadlyImplementedCollistionCheck(itemHandle) && !OutOfBoundsCheck(itemHandle, inventoryRectTransform)){
+                    return true;
+                }
+                else{
+                    itemHandle.Rotate();
+                    itemHandle.rectTransform.localPosition = scanPos + CalculateOddOffset(itemHandle);
+                    if(!VeryBadlyImplementedCollistionCheck(itemHandle) && !OutOfBoundsCheck(itemHandle, inventoryRectTransform)){
+                        return true;
+                    }
+                    itemHandle.Rotate();
+                }
+            }
+        }
+        Destroy(itemHandle.gameObject);
+        return false;        
+    }
+
+    private KuroItemHandle AddItem(KuroItem itemData){
         GameObject newItem = Instantiate(itemPrefab, transform);
         newItem.name = itemData.itemName;
         KuroItemHandle itemHandle = newItem.GetComponent<KuroItemHandle>();
         itemHandle.LoadData(itemData);
+        itemHandle.background.color = defaultColor;
         itemHandle.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, inventoryBounds.width / gridSize.x * itemData.size.x - .5f);
         itemHandle.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, inventoryBounds.width / gridSize.x * itemData.size.y - .5f);
         return itemHandle;
+    }
+
+    private Vector3 CalculateOddOffset(KuroItemHandle itemHandle){
+        Vector3 oddOffset = 
+                Vector3.right * (itemHandle.finalSize.x % 2 == 0 ? tileSize * 0.5f : 0f) +
+                Vector3.up * (itemHandle.finalSize.y % 2 == 0 ? tileSize * 0.5f : 0f);
+        return oddOffset;
     }
 
 
